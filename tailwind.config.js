6738cf9bb9d51e2a453e07804b39d19f0c044058
Fariz Baiquni <farizbaiquni@gmail.com>
2022-05-23 09:43:49 +0700

    add background image below navbar, change text to icon for twitter and facebook, now navbar change ui based on scroll top position

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'main-orange': "url('/src/image/bg-main.jpg')",
+      }
+    },
   },
   plugins: [],
 }
