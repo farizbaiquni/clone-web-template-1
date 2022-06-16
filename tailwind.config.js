e529ff5a15104108b2319bb92f9e2708905ee5c6
Fariz Baiquni <farizbaiquni@gmail.com>
2022-06-16 18:32:46 +0700

    add services and portofolio part and add prettier-taildwinds dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  content: [
-    "./**/*.{html,js}",
-    "./src/**/*.{html,js}"
-  ],
+  content: ["./**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
-        'main-orange': "url('/src/image/bg-main.jpg')",
+        "main-orange": "url('/src/image/bg-main.jpg')",
       },
     },
   },
   plugins: [],
-}
+};
